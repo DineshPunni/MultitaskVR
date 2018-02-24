01517361aeaa5b165db98d6095bc6de3d89f03d8
Dinesh <dinesh.punni@hotmail.de>
2018-02-24 17:29:07 +0100

    Made UI shootable

---
+++ Assets/UIShootable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UIShootable : MonoBehaviour {
+
+
+    public UnityEvent OnButtonShot;
+   
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("bb");
+
+        if(collision.gameObject.tag == "Bullet")
+        {
+            Debug.Log("aaaa");
+            OnButtonShot.Invoke();
+        }
+    }
+}
