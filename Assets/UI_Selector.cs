ef9ba515ebefcb341523eaee7ccdb315c5aa4aea
DineshPunni <dinesh.punni@hotmail.de>
2018-03-05 22:47:05 +0100

    UI_Selector added (raycast shoot)

---
+++ Assets/UI_Selector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Selector : MonoBehaviour {
+
+
+    public LayerMask mask;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, fwd, out hit, 10, mask))
+        {
+            print("ui hit:" + hit.transform.name);
+        }
+	}
+}
